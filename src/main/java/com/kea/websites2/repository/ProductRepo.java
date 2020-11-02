b8830166c62584f33f17f4bd064a0778e77f1709
geor1812 <george.ursachi7@gmail.com>
2020-11-02 15:48:48 +0100

    Added ResourceNotFoundException

--- src/main/java/com/kea/websites2/repository/ProductRepo.java
+++ src/main/java/com/kea/websites2/repository/ProductRepo.java
@@ -3,5 +3,5 @@ package com.kea.websites2.repository;
 import com.kea.websites2.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class ProductRepo extends JpaRepository <Product, Integer> {
+public interface ProductRepo extends JpaRepository<Product, Integer> {
 }
