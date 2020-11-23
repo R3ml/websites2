7753a82ac9608206d8e0cfdec682263847bc1a62
geor1812 <george.ursachi7@gmail.com>
2020-11-23 16:06:15 +0100

    Refactored product controller into product service

--- src/main/java/com/kea/websites2/service/ProductService.java
+++ src/main/java/com/kea/websites2/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.kea.websites2.service;
 
+import com.kea.websites2.exception.ResourceNotFoundException;
 import com.kea.websites2.model.Product;
 import com.kea.websites2.model.utils.PagingHeaders;
 import com.kea.websites2.model.utils.PagingResponse;
@@ -11,7 +12,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.Objects;
@@ -48,6 +52,38 @@ public class ProductService {
 
     public List<Product> getAllProducts(Specification<Product> spec, Sort sort) {
         return repo.findAll(spec, sort);
+    }
+
+    //Get a product by id
+    public ResponseEntity<Product> getProductById(int id) {
+        Product product = repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "id", id));
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    //Create a product
+    public ResponseEntity<Product> createProduct(Product product) {
+        return new ResponseEntity<>(repo.save(product), HttpStatus.CREATED);
+    }
+
+    //Update a product
+    public ResponseEntity<Product> updateProduct(int id, Product product) {
+        Product _product = repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "id", id));
+        _product.setName(product.getName());
+        _product.setPrice(product.getPrice());
+        _product.setType(product.getType());
+        _product.setDescription(product.getDescription());
+        _product.setImgUrl(product.getImgUrl());
+
+        return new ResponseEntity<>(productRepo.save(_product), HttpStatus.OK);
+    }
+
+    //Delete a product
+    public ResponseEntity<HttpStatus> deleteProduct(int id) {
+        repo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
