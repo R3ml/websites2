28419ddb77f8e8a3d4a47529dfc9f2568213a6e1
geor1812 <george.ursachi7@gmail.com>
2020-11-26 17:33:27 +0100

    Navbar

--- Front-End/w2frontend/src/App.js
+++ Front-End/w2frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      console.log(process.env.REACT_APP_NAME);
+      return (
+          <Router>
+            <div className="app">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">Websites2</a>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to={"/products"} className="nav-link">
+                                    Products
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/customers"} className="nav-link">
+                                    Customers
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={"/orders"} className="nav-link">
+                                    Orders
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+          </Router>
+      );
+  }
 }
 
 export default App;
