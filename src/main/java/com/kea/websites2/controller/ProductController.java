9f32d885038f9a62492da7b1bd4aed01840b2f3c
geor1812 <george.ursachi7@gmail.com>
2020-12-09 13:42:29 +0100

    Added CrossOrigin for production server

--- src/main/java/com/kea/websites2/controller/ProductController.java
+++ src/main/java/com/kea/websites2/controller/ProductController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @CrossOrigin(origins = {
         "http://localhost:8081"
+        "https://websites2-frontend.herokuapp.com/"
 })
 @RestController
 public class ProductController {
@@ -43,6 +44,7 @@ public class ProductController {
 
     public HttpHeaders returnHttpHeaders(PagingResponse response) {
         HttpHeaders headers = new HttpHeaders();
+        //exposing the page total so we can access it in react
         headers.set("Access-Control-Expose-Headers", PagingHeaders.PAGE_TOTAL.getName());
         headers.set(PagingHeaders.COUNT.getName(), String.valueOf(response.getCount()));
         headers.set(PagingHeaders.PAGE_SIZE.getName(), String.valueOf(response.getPageSize()));
