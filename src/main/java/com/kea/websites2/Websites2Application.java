eb374097a91970c7d3a89aff068fa9ec7ae9ee66
geor1812 <george.ursachi7@gmail.com>
2020-12-18 14:03:58 +0100

    Trying to deploy to AWS

--- src/main/java/com/kea/websites2/Websites2Application.java
+++ src/main/java/com/kea/websites2/Websites2Application.java
@@ -2,9 +2,10 @@ package com.kea.websites2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Websites2Application {
+public class Websites2Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Websites2Application.class, args);
